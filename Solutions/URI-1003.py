45d833e655ed7fca5c7079df0e88dcfe5fb8f75e
Marufur Rahman <marufurrahman1997@gmail.com>
2018-08-18 10:12:40 +0600

    Accepted: Simple Sum

---
+++ Solutions/URI-1003.py
@@ -0,0 +1,8 @@
+# URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+# Programmed by Marufur Rahman.
+
+A = int(input())
+B = int(input())
+SOMA = A + B
+
+print('SOMA', '=', SOMA)
