c7c74ad5e60c234279e516738ffa000df7b98f1d
Marufur Rahman <marufurrahman1997@gmail.com>
2018-08-18 10:36:09 +0600

    Accepted: Simple Product

---
+++ Solutions/URI-1004.py
@@ -0,0 +1,8 @@
+# URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+# Programmed by Marufur Rahman.
+
+a = int(input())
+b = int(input())
+PROD = a*b
+
+print('PROD =', PROD)
