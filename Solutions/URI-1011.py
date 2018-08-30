bb600ee74b3004b5ce0e1dd988a30b4c99c19f00
MarufurRahman <marufurrahman1997@gmail.com>
2018-08-30 15:23:00 +0600

    Accepted: Sphere

---
+++ Solutions/URI-1011.py
@@ -0,0 +1,10 @@
+# URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1011
+# Programmed by Marufur Rahman.
+
+radius = int(input())
+
+pi = 3.14159
+
+volume = float(4.0 * pi * (radius* radius * radius) / 3)
+
+print("VOLUME = %0.3f" %volume)
