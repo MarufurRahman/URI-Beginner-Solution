0f3ef003231cbab660f7a6560d5afcda320421a2
MarufurRahman <marufurrahman1997@gmail.com>
2018-08-30 15:07:51 +0600

    Accepted: Salary with Bonus

---
+++ Solutions/URI-1009.py
@@ -0,0 +1,10 @@
+# URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1009
+# Programmed by Marufur Rahman.
+
+name = input()
+a = float(input())
+b = float(input())
+bonus = float(b*(15/100))
+total = a + bonus
+
+print('TOTAL = R$ %.2f' %total)
