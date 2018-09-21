dc5fc74c924775c64217bbea79419789cec918a5
marufurrahman <marufurrahman1997@gmail.com>
2018-09-21 15:52:35 +0600

    Accepted: Consumption

---
+++ Solutions/URI-1014.py
@@ -0,0 +1,9 @@
+# URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1014
+# Programmed by Marufur Rahman.
+
+a = int(input())
+b = float(input())
+
+consum = a/b
+
+print("%0.3f km/l" %consum)
