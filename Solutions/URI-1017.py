ed3b068832ff25db393b30ba5795e2600a3a8e0f
marufurrahman <marufurrahman1997@gmail.com>
2018-10-08 23:11:44 +0600

    Accepted: Fuel Spent

---
+++ Solutions/URI-1017.py
@@ -0,0 +1,9 @@
+# URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1017
+# Programmed by Marufur Rahman.
+
+spentTime = int(input())
+averageSpeed = int(input())
+total = (spentTime * averageSpeed)
+result = total/12
+
+print("%.3f" %result)
