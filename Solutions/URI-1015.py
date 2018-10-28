9bade50da083c3c0c5eed5a48412d952318450e1
Afzalur Rahaman <44163823+7afzal@users.noreply.github.com>
2018-10-28 15:33:57 +0600

    Update URI-1015.py

--- Solutions/URI-1015.py
+++ Solutions/URI-1015.py
@@ -1,7 +1,30 @@
 # URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1015
 # Programmed by Marufur Rahman.
+##Read the four values corresponding to the x and y axes of two points in the plane, p1 (x1, y1) and p2 (x2, y2) and calculate the distance between them, showing four decimal places after the comma, according to the formula:
 
-import math
+Distance = 
+
+Input
+
+The input file contains two lines of data. The first one contains two double values: x1 y1 and the second one also contains two double values with one digit after the decimal point: x2 y2.
+
+Output
+
+Calculate and print the distance value using the provided formula, with 4 digits after the decimal point.
+
+Input Sample
+
+1.0  7.0
+5.0  9.0
+
+Output Sample
+
+4.4721
+
+
+#Solution : 
+
+
 
 var1 = input().split(" ")
 var2 = input().split(" ")
