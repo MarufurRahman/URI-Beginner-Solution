7937a5b744331c6881298fee4a372d6b2fa67456
Afzalur Rahaman <44163823+7afzal@users.noreply.github.com>
2018-10-28 15:09:33 +0600

    Update URI-1017.py

--- Solutions/URI-1017.py
+++ Solutions/URI-1017.py
@@ -1,5 +1,25 @@
+
 # URI Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1017
 # Programmed by Marufur Rahman.
+#Little John wants to calculate and show the amount of spent fuel liters on a trip, using a car that does 12 Km/L. For this, he would like you to help him through a simple program. To perform the calculation, you have to read spent time (in hours) and the same average speed (km/h). In this way, you can get distance and then, calculate how many liters would be needed. Show the value with three decimal places after the point.
+
+Input
+The input file contains two integers. The first one is the spent time in the trip (in hours). The second one is the average speed during the trip (in Km/h).
+
+Output
+Print how many liters would be needed to do this trip, with three digits after the decimal point.
+
+Input
+10
+85
+Output:
+
+70.833
+
+##Solution:
+
+
+
 
 spentTime = int(input())
 averageSpeed = int(input())
@@ -7,3 +27,4 @@ total = (spentTime * averageSpeed)
 result = total/12
 
 print("%.3f" %result)
+
